42996bd94adfefdd93afeca2f723ec8b2acb79de
David Evan <de14768X@cimbniaga.co.id>
2024-11-20 15:55:31 +0700

    initial commit

---
+++ src/main/java/id/co/cimbniaga/financialproductmanagement/FinancialProductManagementApplication.java
@@ -0,0 +1,13 @@
+package id.co.cimbniaga.financialproductmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialProductManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialProductManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/co/cimbniaga/financialproductmanagement/FinancialProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.cimbniaga.financialproductmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialProductManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
