e1bfb83702f794e69ccbe24c8891c70922fd0684
cempakarahmaa <crahmadavi@gmail.com>
2024-11-21 11:11:59 +0700

    update User Controller (add response)

--- src/main/java/id/co/cimbniaga/financialproductmanagement/controller/UserController.java
+++ src/main/java/id/co/cimbniaga/financialproductmanagement/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 
 @RestController
@@ -27,7 +26,7 @@ public class UserController {
             @RequestBody UserRequestDTO userRequestDTO) {
         try {
             User user = userService.registerUser(userRequestDTO);
-            return new ResponseEntity<>(user, HttpStatus.CREATED);
+            return new ResponseEntity<>("Register Berhasil", HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -40,7 +39,11 @@ public class UserController {
     ) {
         try {
             boolean isLogin = userService.LoginUser(userRequestDTO);
-            return new ResponseEntity<>(isLogin, HttpStatus.OK);
+            if(isLogin) {
+                return new ResponseEntity<>("Login Berhasil", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Login Gagal", HttpStatus.UNAUTHORIZED);
+            }
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
