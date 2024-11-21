3be8a058eddf8bbce9926464d9451882b9417220
Ivan Febrianto <if14618X@cimbniaga.co.id>
2024-11-21 17:07:45 +0700

    changes in query, add where condition activity_type = update

--- src/main/java/id/co/cimbniaga/financialproductmanagement/controller/UserController.java
+++ src/main/java/id/co/cimbniaga/financialproductmanagement/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/auth")
+    @RequestMapping("/api/auth")
 public class UserController {
 
     private final UserService userService;
